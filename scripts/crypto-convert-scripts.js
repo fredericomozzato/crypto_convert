d5cb7d1f40cf95595d4672b98bec0b66c177c739
Frederico Mozzato <frederico.smozzato@gmail.com>
2023-06-13 19:47:17 -0300

    Change select elements to text forms and datalist

--- scripts/crypto-convert-scripts.js
+++ scripts/crypto-convert-scripts.js
@@ -11,16 +11,19 @@ let conversionType = true;
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    const cryptoList = document.getElementById("cryptoList");
-    const fiatList = document.getElementById("fiatList");
+    const cryptoInput = document.getElementById("crypto-input");
+    const fiatInput = document.getElementById("fiat-input");
 
-    cryptoList.onchange = function() {
-        crypto = this.value;
-    }
+    const cryptoList = document.getElementById("crypto-list");
+    const fiatList = document.getElementById("fiat-list");
 
-    fiatList.onchange = function() {
-        fiat = this.value;
-    }
+    // cryptoInput.onchange = function() {
+    //     crypto = this.value;
+    // }
+
+    // fiatInput.onchange = function() {
+    //     fiat = this.value;
+    // }
 
     document.getElementById("swap").onclick = function() {
         const conversionInfo = document.getElementById("conversion-info");
@@ -32,33 +35,53 @@ document.addEventListener("DOMContentLoaded", function() {
             // convert from fiat to crypto
             conversionType = false;
             conversionInfo.innerHTML = "Fiat to Crypto";
-            convertFromElement.appendChild(fiatList);
-            convertToElement.appendChild(cryptoList);
+            convertFromElement.appendChild(fiatInput);
+            convertToElement.appendChild(cryptoInput);
 
         } else {
             // convert from crypto to fiat
             conversionType = true;
             conversionInfo.innerHTML = "Crypto to Fiat";
-            convertFromElement.appendChild(cryptoList);
-            convertToElement.appendChild(fiatList);
+            convertFromElement.appendChild(cryptoInput);
+            convertToElement.appendChild(fiatInput);
         }
     }
 
     document.getElementById("convert").onclick = async function() {
-        crypto = cryptoList.value;
-        fiat = fiatList.value;
+        crypto = cryptoInput.value;
+        fiat = fiatInput.value;
 
-        // grab tickers from crypto 
-        const selectedCrypto = cryptoList.options[cryptoList.selectedIndex];
+        console.log(`crypto = ${crypto} | fiat = ${fiat}`);
+        
+        // compare crypto and fiat with ever option on crypto-list and fiat-list to find selected
+        let selectedCrypto;
+        let selectedFiat;
+
+        Array.from(cryptoList.options).forEach(o => {
+            if (o.dataset.ticker === crypto) {
+                selectedCrypto = o;
+            }
+        })
+
+        Array.from(fiatList.options).forEach(o => {
+            if (o.dataset.ticker === fiat) {
+                selectedFiat = o;
+            }
+        })
+
+        // get ids
+        const cryptoId =  selectedCrypto.dataset.id;
+        const fiatId = selectedFiat.dataset.id;
+
+        // get tickers
         const cryptoTicker = selectedCrypto.dataset.ticker;
-
-        // grab ticker from fiat
-        const selectedFiat = fiatList.options[fiatList.selectedIndex];
         const fiatTicker = selectedFiat.dataset.ticker;
+        
+           
 
-        const response = await fetch(`${apiAddress}ids=${crypto}&vs_currencies=${fiat}&precision=3`);
+        const response = await fetch(`${apiAddress}ids=${cryptoId}&vs_currencies=${fiatId}&precision=3`);
         const data = await response.json();    
-        rate = data[crypto][fiat];
+        rate = data[cryptoId][fiatId];
     
         const amount = document.getElementById("amount").value;
         let conversion;
